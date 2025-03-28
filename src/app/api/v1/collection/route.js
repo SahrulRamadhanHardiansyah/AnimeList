d472682846330f8debe1da760e9803737787ea38
Sahrul Ramadhan Hardiansyah <sahrulrmdh3@gmail.com>
2025-03-28 18:56:54 +0700

    fix bugs

--- src/app/api/v1/collection/route.js
+++ src/app/api/v1/collection/route.js
@@ -1,14 +1,15 @@
 import prisma from "@/libs/prisma";
 
 export async function POST(request) {
-  const { anime_mal_id, user_email, anime_image, anime_title } = await request.json();
-  const data = { anime_mal_id, user_email, anime_image, anime_title };
+  try {
+    const { anime_mal_id, user_email, anime_image, anime_title } = await request.json();
+    const data = { anime_mal_id, user_email, anime_image, anime_title };
 
-  const createCollection = await prisma.collection.create({ data });
+    const createCollection = await prisma.collection.create({ data });
 
-  if (!createCollection) {
-    return Response.json({ status: 500, isCreated: false });
-  } else {
-    return Response.json({ status: 200, isCreated: true });
+    return Response.json({ status: 200, isCreated: true, data: createCollection });
+  } catch (error) {
+    console.error("Error creating collection:", error);
+    return Response.json({ status: 500, isCreated: false, error: error.message });
   }
 }
